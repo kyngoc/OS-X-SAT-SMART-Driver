0785140011fe5296329da5237b9259c207dc0383
kyngoc <macosx@Macs-MacBook-Pro.local>
2013-08-27 23:12:11 +0700

    Revert 52a1801..5c76e2a
    This rolls back to commit 52a1801fff742a68290e2d12c66478b3e76a1e4a.

--- Resources/postinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-chmod -R go-w /System/Library/Extensions/SATSMARTDriver.kext /System/Library/Extensions/SATSMARTLib.plugin
-chown -R root:wheel /System/Library/Extensions/SATSMARTDriver.kext /System/Library/Extensions/SATSMARTLib.plugin
-kextutil /System/Library/Extensions/SATSMARTDriver.kext
-exit 0
